40821e16e36bd64345f901ad1a39c0e1f7d7c924
Ehsan <ehsan.shirzadi@gmail.com>
2015-10-15 22:53:26 +0330

    First

--- nic/generate_domains.py
+++ nic/generate_domains.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("/root/projects/services")
 from public_data import db
 
+
 col = db['ir_domains']
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
