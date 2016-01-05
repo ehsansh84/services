eba59ffadc30b17156e19a491a6e6a80d4fe17a1
Ehsan <ehsan.shirzadi@gmail.com>
2016-01-05 12:10:29 +0330

    First

--- news_project/crawler.py
+++ news_project/crawler.py
@@ -15,15 +15,19 @@ feed = feedparser.parse(rss_link)
 # print(feed["version"])
 # print(feed["channel"])
 # print(feed["items"])
+
 for item in feed["items"]:
     print(item)
     news_item = {
         'title': item['title'],
         'summary': item['summary'],
         # 'date': item['date'],
-        'date_parsed': item['date_parsed'],
+        # 'date_parsed': item['date_parsed'],
         'link': item['link'],
         'text': ''
     }
 
+    print(news_item)
+
     col_news.insert(news_item)
+
