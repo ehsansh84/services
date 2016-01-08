e9e53cc73981387d344c053bfeb545fa66b6958d
Ehsan <ehsan.shirzadi@gmail.com>
2016-01-08 22:15:42 +0330

    First

--- news_project/crawler.py
+++ news_project/crawler.py
@@ -2,12 +2,95 @@ __author__ = 'Ehsan'
 import sys
 sys.path.append("/root/ehsan/services")
 from public_data import *
-rss_link = 'http://www.nytimes.com/services/xml/rss/nyt/MiddleEast.xml'
-col_news = db['news']
-
 import feedparser
 
-feed = feedparser.parse(rss_link)
+# rss_link = 'http://www.nytimes.com/services/xml/rss/nyt/MiddleEast.xml'
+rss_links = [
+    {'source': 'nytimes', 'category': 'world', 'sub_category': ['world'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/World.xml'},
+    {'source': 'nytimes', 'category': 'world', 'sub_category': ['africa'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Africa.xml'},
+    {'source': 'nytimes', 'category': 'world', 'sub_category': ['americas'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Americas.xml'},
+    {'source': 'nytimes', 'category': 'world', 'sub_category': ['asia pacific'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml'},
+    {'source': 'nytimes', 'category': 'world', 'sub_category': ['europe'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Europe.xml'},
+    {'source': 'nytimes', 'category': 'world', 'sub_category': ['middle east'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/MiddleEast.xml'},
+    {'source': 'nytimes', 'category': 'U.S', 'sub_category': ['U.S'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/US.xml'},
+    {'source': 'nytimes', 'category': 'U.S', 'sub_category': ['education'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Education.xml'},
+    {'source': 'nytimes', 'category': 'U.S', 'sub_category': ['politics'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Politics.xml'},
+    {'source': 'nytimes', 'category': 'N.Y./Region', 'sub_category': ['N.Y./Region'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/NYRegion.xml'},
+    {'source': 'nytimes', 'category': 'N.Y./Region', 'sub_category': ['City Room Blog'], 'link': 'http://cityroom.blogs.nytimes.com/feed/'},
+    {'source': 'nytimes', 'category': 'N.Y./Region', 'sub_category': ['Fort Greene, NY Blog'], 'link': 'http://fort-greene.blogs.nytimes.com/feed'},
+    {'source': 'nytimes', 'category': 'N.Y./Region', 'sub_category': ['East Village Blog'], 'link': 'http://eastvillage.thelocal.nytimes.com/feed/'},
+    {'source': 'nytimes', 'category': 'Business', 'sub_category': ['Business'], 'link': 'http://feeds.nytimes.com/nyt/rss/Business'},
+    {'source': 'nytimes', 'category': 'Business', 'sub_category': ['Small Business'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/SmallBusiness.xml'},
+    {'source': 'nytimes', 'category': 'Business', 'sub_category': ['DealBook'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Dealbook.xml'},
+    {'source': 'nytimes', 'category': 'Business', 'sub_category': ['Energy & Environment'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/EnergyEnvironment.xml'},
+    {'source': 'nytimes', 'category': 'Business', 'sub_category': ['Media & Advertising'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/MediaandAdvertising.xml'},
+    {'source': 'nytimes', 'category': 'Business', 'sub_category': ['International Business'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/InternationalBusiness.xml'},
+    {'source': 'nytimes', 'category': 'Business', 'sub_category': ['Economy'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Economy.xml'},
+    {'source': 'nytimes', 'category': 'Business', 'sub_category': ['Your Money'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/YourMoney.xml'},
+    {'source': 'nytimes', 'category': 'Technology', 'sub_category': ['Technology'], 'link': 'http://feeds.nytimes.com/nyt/rss/Technology'},
+    {'source': 'nytimes', 'category': 'Technology', 'sub_category': ['Bits Blog'], 'link': 'http://bits.blogs.nytimes.com/feed/'},
+    {'source': 'nytimes', 'category': 'Technology', 'sub_category': ['Personal Tech'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/PersonalTech.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Sports'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Sports.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['College Football'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/CollegeFootball.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Pro-Football'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/ProFootball.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['International Sports'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/InternationalSports.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Golf'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Golf.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Soccer'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Soccer.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Baseball'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Baseball.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Hockey'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Hockey.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Tennis'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/Tennis.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['College Basketball'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/CollegeBasketball.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Pro-Basketball'], 'link': 'http://www.nytimes.com/services/xml/rss/nyt/ProBasketball.xml'},
+    {'source': 'nytimes', 'category': 'Sport', 'sub_category': ['Gambit Blog'], 'link': 'http://gambit.blogs.nytimes.com/feed/'},
+    # {'source': 'nytimes', 'category': 'Sport', 'sub_category': [], 'link': ''},
+    # {'source': 'nytimes', 'category': 'Sport', 'sub_category': [], 'link': ''},
+]
+col_news = db['news']
+
+
+def exists(link):
+    results = col_news.find({'link': link})
+    if results.count() <> 0:
+        for item in results:
+            print('ITEM:', item)
+            return item['sub_category']
+    else:
+        return 0
+
+
+def fetch(rss_item):
+    feed = feedparser.parse(rss_item['link'])
+    for item in feed["items"]:
+        news_item = {
+            'source': rss_item['source'],
+            'category': rss_item['category'],
+            'sub_category': rss_item['sub_category'],
+            'title': item['title'],
+            'summary': item['summary'],
+            'link': item['link'],
+            'text': ''
+            # 'date': item['date'],
+            # 'date_parsed': item['date_parsed'],
+        }
+        t_item = exists(news_item['link'])
+        if t_item == 0:
+            col_news.insert(news_item)
+        else:
+            if news_item['sub_category'] in t_item:
+                print('DUP news')
+            else:
+                news_item['sub_category'].append(t_item)
+                col_news.update_one({'link': news_item['link']}, {
+                    "$set": {'sub_category': news_item['sub_category']}
+                })
+
+print('Total news is:'),
+print(col_news.count())
+for item in rss_links:
+    print(item['category'])
+    fetch(item)
+print('Total news is:'),
+print(col_news.count())
 
 
 # print(feed["bozo"])
@@ -15,19 +98,4 @@ feed = feedparser.parse(rss_link)
 # print(feed["version"])
 # print(feed["channel"])
 # print(feed["items"])
-
-for item in feed["items"]:
-    print(item)
-    news_item = {
-        'title': item['title'],
-        'summary': item['summary'],
-        # 'date': item['date'],
-        # 'date_parsed': item['date_parsed'],
-        'link': item['link'],
-        'text': ''
-    }
-
-    print(news_item)
-
-    col_news.insert(news_item)
 
--- social/facebook.py
+++ social/facebook.py
@@ -3,11 +3,11 @@ __author__ = 'Ehsan'
 from facebook import Facebook
 
 api_key = '1523315421330779'
-secret  = 'f2400983f41838547108ddad99fd5585'
+secret = 'f2400983f41838547108ddad99fd5585'
 
-session_key = 'your infinite Session key of user'
+# session_key = 'your infinite Session key of user'
 
 fb = Facebook(api_key, secret)
-fb.session_key = session_key
+# fb.session_key = session_key
 
 # now use the fb object for playing around
