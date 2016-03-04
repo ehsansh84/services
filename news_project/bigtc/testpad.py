bd29ad1e0d7cfcd3f51a484ca327d6201bda2e8e
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-05 02:27:32 +0330

    ok

--- news_project/bigtc/testpad.py
+++ news_project/bigtc/testpad.py
@@ -1,8 +1,11 @@
 import sys
+import re
 sys.path.append("/root/ehsan/services")
 from public_data import *
 col = db['news']
 results = col.find({}).limit(100)
 for r in results:
     # print(r['title'])
-    print(r['summary'])
+    summary = r['summary']
+    summary = re.sub("<.*?>", "", summary)
+    print(summary)
