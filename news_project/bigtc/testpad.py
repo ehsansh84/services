af627602ef55dc2415afcb3b02ac4a6596cc846f
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-05 02:33:02 +0330

    ok

--- news_project/bigtc/testpad.py
+++ news_project/bigtc/testpad.py
@@ -4,8 +4,12 @@ sys.path.append("/root/ehsan/services")
 from public_data import *
 col = db['news']
 results = col.find({}).limit(100)
+output = open('output.txt', 'w')
+
 for r in results:
     # print(r['title'])
     summary = r['summary']
     summary = re.sub("<.*?>", "", summary)
     print(summary)
+    output.write(summary)
+output.close()
