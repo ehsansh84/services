80f5be5155082f743475d496e220b7b49445fa60
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-05 04:15:56 +0330

    ok

--- news_project/bigtc/testpad.py
+++ news_project/bigtc/testpad.py
@@ -3,9 +3,10 @@ import re
 sys.path.append("/root/ehsan/services")
 from public_data import *
 col = db['news']
+col_temp = db['temp']
 results = col.find({}).limit(100)
 output = open('output.txt', 'w')
-
+data = ''
 for r in results:
     # print(r['title'])
     summary = r['summary']
@@ -13,4 +14,6 @@ for r in results:
     print(summary)
     output.write(summary.encode('utf-8'))
     output.write('\n')
+    data += summary.encode('utf-8') + '\n'
 output.close()
+col_temp.insert({'data': data})
