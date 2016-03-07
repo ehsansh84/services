03fac40143a5bdc2a1141c5eba440951e50c146f
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-07 06:16:17 +0330

    ok

--- news_project/bigtc/statistics.py
+++ news_project/bigtc/statistics.py
@@ -13,27 +13,30 @@ sub_categories = []
 i = 0
 news = col.find({})
 
-# try:
-#     for item in news:
-#         if not item['category'] in categories:
-#             categories.append(item['category'])
-#
-#         if not item['sub_category'] in sub_categories:
-#             sub_categories.append(item['sub_category'])
-#         i += 1
-#
-#         if i % 1000 == 0:
-#             print(i)
-# except Exception, e:
-#     print(e.message)
+try:
+    for item in news:
+        if not item['category'] in categories:
+            categories.append(item['category'])
+
+        if len(item['sub_category']) == 1:
+            if not item['sub_category'] in sub_categories:
+                sub_categories.append(item['sub_category'])
+        else:
+            print(item['sub_category'])
+        i += 1
+
+        if i % 1000 == 0:
+            print(i)
+except Exception, e:
+    print(e.message)
 
 
 # for item in categories:
 #     col_categories.insert({'name': item})
+for item in sub_categories:
+    col_sub_categories.insert({'name': item})
 
-r_categories = col_categories.find({})
-for category in r_categories:
-    news_count = col.count({'category': category['name']})
-    col_categories.update({'name': category['name']}, {'count': news_count})
-    # print(news_count),
-    # print(category)
+# r_categories = col_categories.find({})
+# for category in r_categories:
+#     news_count = col.count({'category': category['name']})
+#     col_categories.update({'name': category['name']}, {'count': news_count})
