96f64a87e7e45d121ba8ff28644c01a1f4205be4
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-07 05:37:09 +0330

    ok

--- news_project/bigtc/statistics.py
+++ news_project/bigtc/statistics.py
@@ -11,18 +11,19 @@ i = 0
 news = col.find({})
 for item in news:
     if not item['category'] in categories:
+        print('Cur Cat: %s Cats: %s' % (item['category'], categories))
         categories.append(item['category'])
-    if not item['sub_category'] in sub_categories:
-        categories.append(item['sub_category'])
+    # if not item['sub_category'] in sub_categories:
+    #     categories.append(item['sub_category'])
     i += 1
-    if i % 1000 == 0:
-        print(i)
-    if i % 10000 == 0:
+    # if i % 1000 == 0:
+    #     print(i)
+    if i % 10 == 0:
         break
 
 print('CATS:')
 print(categories)
-print('SUB CATS:')
-print(sub_categories)
+# print('SUB CATS:')
+# print(sub_categories)
 
 
