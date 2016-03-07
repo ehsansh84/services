64b3b03e88c3bfe9499da57cdbdd29d8318e17cb
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-07 05:29:55 +0330

    ok

---
+++ news_project/bigtc/statistics.py
@@ -0,0 +1,24 @@
+from public_data import *
+
+col = db['news']
+
+categories = []
+sub_categories = []
+
+i = 0
+news = col.find({})
+for item in news:
+    if not item['category'] in categories:
+        categories.append(item['category'])
+    if not item['sub_category'] in sub_categories:
+        categories.append(item['sub_category'])
+    i += 1
+    if i % 1000 == 0:
+        print(i)
+
+print('CATS:')
+print(categories)
+print('SUB CATS:')
+print(sub_categories)
+
+
