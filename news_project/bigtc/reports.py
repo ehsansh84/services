1b64f10fc21f4e440dba9a31bfbce403707dea7c
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-09 05:04:25 +0330

    ok

--- news_project/bigtc/reports.py
+++ news_project/bigtc/reports.py
@@ -8,7 +8,10 @@ def rss_statistics():
     rss_list = col_rss.find()
     total_time = 0
     for item in rss_list:
-        total_time += item['duration']
+        try:
+            total_time += item['duration']
+        except Exception, ex:
+            print(ex.message)
     print(total_time)
 
 rss_statistics()
