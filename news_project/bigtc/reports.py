694f02c7dffa8ac6a6343b762d879d659ee81620
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-09 05:08:59 +0330

    ok

--- news_project/bigtc/reports.py
+++ news_project/bigtc/reports.py
@@ -7,11 +7,13 @@ col_rss = db['rss']
 def rss_statistics():
     rss_list = col_rss.find()
     total_time = 0
+    rss_count = len(rss_list)
     for item in rss_list:
         try:
             total_time += item['duration']
         except Exception, ex:
             print(ex.message)
     print(total_time)
+    print(rss_count)
 
 rss_statistics()
