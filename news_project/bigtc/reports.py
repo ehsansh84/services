42b8b8ff52439a52624aa73f1c193eaecf1e99a7
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-09 05:02:05 +0330

    ok

---
+++ news_project/bigtc/reports.py
@@ -0,0 +1,12 @@
+from public_data import *
+
+col_rss = db['rss']
+
+def rss_statistics():
+    rss_list = col_rss.find()
+    total_time = 0
+    for item in rss_list:
+        total_time += item['duration']
+    print(total_time)
+
+rss_statistics()
