4ad1ca6e6818316680683014e8a6dce818feab7e
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-11 11:36:25 +0330

    ok

--- news_project/bigtc/statistics.py
+++ news_project/bigtc/statistics.py
@@ -113,7 +113,7 @@ def extract_sources():
         if not item['source'] in source_list:
             source_list.append(item['source'])
     for item in source_list:
-        col_sources.insert({item.encode('utf-8')})
+        col_sources.insert({str(item)})
     print(source_list)
 # cat_mapping()
 
