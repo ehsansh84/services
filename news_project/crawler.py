b399ee18f810366896c1f7a42c2dfdcc10e188f4
Ehsan <ehsan.shirzadi@gmail.com>
2016-03-24 22:42:05 +0430

    ok

--- news_project/crawler.py
+++ news_project/crawler.py
@@ -42,69 +42,72 @@ def exists(link):
 
 
 def fetch(rss_item):
-    t.start()
-    dup_count = 0
-    new_count = 0
-    feed = feedparser.parse(rss_item['link'])
-    for item in feed["items"]:
-        # t.start()
-        news_item = {
-            'source': rss_item['source'],
-            'category': rss_item['category'],
-            'sub_category': rss_item['sub_category'],
-            'title': item['title'],
-            'su mmary': item['summary'],
-            'link': item['link'],
-            'text': ''
-            # 'date': item['date'],
-            # 'date_parsed': item['date_parsed'],
-        }
-        # t1 = timer()
-        # t1.start()
-        t_item = exists(news_item['link'])
-        # exist_time = t1.end()
-
-        if t_item == 0:
-            # print('t_ITEM is 0')
+    try:
+        t.start()
+        dup_count = 0
+        new_count = 0
+        feed = feedparser.parse(rss_item['link'])
+        for item in feed["items"]:
             # t.start()
-            col_news.insert(news_item)
-            # print('Took %s seconds for insert!' % t.end())
-            new_count += 1
-        else:
-            # if t_item in news_item['sub_category']:
-            dup_count += 1
-                # print('DUP news')
-            # else:
-            #     print('===========================================================')
-            #     print('SUB_CAT:', news_item['sub_category']),
-            #     print('ITEM:', t_item)
-            #     news_item['sub_category'].append(t_item)
-            #     col_news.update_one({'link': news_item['link']}, {
-            #         "$set": {'sub_category': news_item['sub_category']}
-            #     })
-                # exit()
-        # print('Total %s seconds - Check Exists %s' % (t.end(), exist_time))
-    duration = t.end()
-    link = rss_item['link']
-    print('DUP: %s NEW: %s TIME: %s SOURCE: %s LINK: %s' % (dup_count, new_count, duration, rss_item['category'], link))
-    total_count = new_count + dup_count
-    last_read = datetime.now()
-    col_rss.update({'link': link}, {"$set": {
-        'total_count': total_count,
-        'duplicates': dup_count,
-        'new': new_count,
-        'last_read': last_read,
-        'duration': duration
-    }})
+            news_item = {
+                'source': rss_item['source'],
+                'category': rss_item['category'],
+                'sub_category': rss_item['sub_category'],
+                'title': item['title'],
+                'su mmary': item['summary'],
+                'link': item['link'],
+                'text': ''
+                # 'date': item['date'],
+                # 'date_parsed': item['date_parsed'],
+            }
+            # t1 = timer()
+            # t1.start()
+            t_item = exists(news_item['link'])
+            # exist_time = t1.end()
 
-    col_rss_log.insert({
-        'link': link,
-        'total_count': total_count,
-        'duplicates': dup_count,
-        'new': new_count,
-        'last_read': last_read,
-        'duration': duration
-    })
+            if t_item == 0:
+                # print('t_ITEM is 0')
+                # t.start()
+                col_news.insert(news_item)
+                # print('Took %s seconds for insert!' % t.end())
+                new_count += 1
+            else:
+                # if t_item in news_item['sub_category']:
+                dup_count += 1
+                    # print('DUP news')
+                # else:
+                #     print('===========================================================')
+                #     print('SUB_CAT:', news_item['sub_category']),
+                #     print('ITEM:', t_item)
+                #     news_item['sub_category'].append(t_item)
+                #     col_news.update_one({'link': news_item['link']}, {
+                #         "$set": {'sub_category': news_item['sub_category']}
+                #     })
+                    # exit()
+            # print('Total %s seconds - Check Exists %s' % (t.end(), exist_time))
+        duration = t.end()
+        link = rss_item['link']
+        print('DUP: %s NEW: %s TIME: %s SOURCE: %s LINK: %s' % (dup_count, new_count, duration, rss_item['category'], link))
+        total_count = new_count + dup_count
+        last_read = datetime.now()
+        col_rss.update({'link': link}, {"$set": {
+            'total_count': total_count,
+            'duplicates': dup_count,
+            'new': new_count,
+            'last_read': last_read,
+            'duration': duration
+        }})
+
+        col_rss_log.insert({
+            'link': link,
+            'total_count': total_count,
+            'duplicates': dup_count,
+            'new': new_count,
+            'last_read': last_read,
+            'duration': duration
+        })
+    except:
+        log.color_print(text=log.get_exception(), color=Color.YELLOW)
 
 total_count_old = col_news.count()
 error_count = 0
