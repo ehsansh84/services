46ded692cbe2fd76775fabf7ea5de3b0a8bf23d8
Ehsan <ehsan.shirzadi@gmail.com>
2016-04-01 21:13:33 +0430

    ok

--- news_project/bigtc/testpad.py
+++ news_project/bigtc/testpad.py
@@ -130,7 +130,7 @@ def news_text_fetch_old():
     doc = urllib2.urlopen(link)
     soup = BeautifulSoup(doc, 'html.parser')
     log.color_print(color=Color.CYAN, text=soup)
-    news_area = soup.select('div[itemprop="articleBody"]')
+    news_area = soup.select('div[itemprop="articleBody"]')[0]
     log.color_print(color=Color.RED, text=news_area)
     for script in news_area(["script", "style"]):
         script.extract()
