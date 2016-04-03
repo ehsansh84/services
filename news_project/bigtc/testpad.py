2d5c50d300545f9c0a7e93c6f91e87d328de5715
Ehsan <ehsan.shirzadi@gmail.com>
2016-04-03 11:33:08 +0430

    ok

--- news_project/bigtc/testpad.py
+++ news_project/bigtc/testpad.py
@@ -234,7 +234,7 @@ def create_output_excel():
 
         news = col.find({'category': {'$ne': 'Unknown'}, 'text': {'$ne': ''}}).limit(10)
         for item in news:
-            worksheet.write('A' + str(row), item['_id'])
+            worksheet.write('A' + str(row), str(item['_id']))
             worksheet.write('B' + str(row), item['category'])
             worksheet.write('C' + str(row), item['text'])
             row += 1
