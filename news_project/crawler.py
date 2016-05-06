15db6d735167fb89bede1ef7170cd79654e41359
Ehsan <ehsan.shirzadi@gmail.com>
2016-05-06 20:58:40 +0430

    ok

--- news_project/crawler.py
+++ news_project/crawler.py
@@ -14,6 +14,7 @@ col_errors_log = db['errors_log']
 t = timer()
 total_time = timer()
 hours_elapsed = 2
+
 query_before_time = dt.datetime.now() - dt.timedelta(hours=2)
 
 log.color_print(text='processing %s RSS' % rss_count, color=Color.BLUES)
